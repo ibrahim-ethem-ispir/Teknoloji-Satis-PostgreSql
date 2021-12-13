a97cf08958343967b36294b7923e66f512837bb4
ibrahim ethem ispir <ethem6046@gmail.com>
2021-12-13 21:15:30 +0300

    veritabanında olan mail ile tekrar kayıt olunmasın

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -25,20 +25,38 @@ registerPost = async (req,res) => {
     }
     else{  
         console.log("Hata mesajı yok Kayıt bölümünde else içerisinde")
-        adminRegistration.create({
-            name: req.body.name,
-            surname: req.body.surname,
-            email: req.body.email,
-            password: req.body.password
-        })
-            .then((result) => {
-                console.log("Registration Successfully Added")
+        try {
+            const _admin = await adminRegistration.findOne({where: {email:req.body.email}})
+            if (_admin) {
+                req.flash("validationError", [{msg : "this email is in use"}])
+                req.flash("name", req.body.name)
+                req.flash("surname", req.body.surname)
+                req.flash("email", req.body.email)
+                req.flash("password", req.body.password)
+                req.flash("passwordAgain", req.body.passwordAgain)
                 res.redirect("/register")
-
-            })
-            .catch((err) => {
-                console.log("Error Output ===  "+err)
-            })   
+            }
+            else{
+                adminRegistration.create({
+                    name: req.body.name,
+                    surname: req.body.surname,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+                    .then((result) => {
+                        console.log("Registration Successfully Added")
+                        res.redirect("/register")
+        
+                    })
+                    .catch((err) => {
+                        console.log("Error Output ===  "+err)
+                    })   
+            }
+        } catch (err) {
+            console.log("registration is incorrect")
+        }
+        
+        
     }    
 }
 
